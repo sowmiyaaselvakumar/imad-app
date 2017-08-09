bcc7073f959d4702e1f5ef0447ee46d7f2926115
sowmiyaaselvakumar <30617879+sowmiyaaselvakumar@users.noreply.github.com>
2017-08-09 12:36:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+.get('/ui/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'favicon.ico'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
