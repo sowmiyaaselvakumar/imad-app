e52b0a8737a16e5d1a3e7696afd332db7efe84d8
sowmiyaaselvakumar <30617879+sowmiyaaselvakumar@users.noreply.github.com>
2017-08-12 10:19:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
 console.log('Loaded!');
 
-
 var element = document.getElementById('main-text');
 element.innerHtml='EXO SEHUN';
+
+//move the image
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+  img.style.marginLeft ='100px' 
+};
